f99a7d15e927f2a59089b1703724386ad2e9349b
paguirre82 <pablod@teambaw.com.au>
2022-08-23 19:36:12 +1000

    Now we have many messages

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!"); Console.WriteLine("Hello, C# Academy! One more modification!");
+
